337041f8fdde5eeec85c45f6fefe4b9ea027d592
zephrxq <96728724+OmegaOrbitals@users.noreply.github.com>
2026-04-27 21:06:56 +0800

    Added song searching and adding to queue

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -4,7 +4,9 @@
 
     let socket;
     let roomId = $state();
-    let roomData = $state({})
+    let roomData = $state({});
+    let searchQuery = state();
+    let searchResults = state([]);
 
     $effect(() => {
         socket = io("localhost:3000");
@@ -15,19 +17,41 @@
             if(joinResult.status == "success") {
                 roomData = joinResult.roomData;
                 joinRoom();
-            }
-            if(joinResult.status == "fail") {
+            } else if(joinResult.status == "fail") {
                 alert("Failed ", joinResult.reason);
             }
         })
     }
     
     function joinRoom() {
-        alert("Joined room ", roomData);
+        alert("Joined room ");
+        console.log(roomData)
     }
 
+    function search() {
+        if(!roomId || !searchQuery) return;
+
+        socket.emit("search", searchQuery, (data) => {
+            searchResults = data;
+        })
+    }
+
+    function addSong(songData) {
+        socket.emit("add-song", songData);
+    }
 </script>
 
 <h1>Jelly</h1>
 <input bind:value={roomId}>
 <button onclick={tryJoinRoom}>Join room</button>
+<input bind:value={searchQuery}>
+<button onclick={search}>Search</button>
+<div id="searchResults">
+    {#each searchResults as searchResult, index}
+        <div id="searchResult">
+            <p>{searchResult.snippet.channelTitle}</p>
+            <p>{searchResult.snippet.title}</p>
+            <button onclick={addSong(searchResult)}>Add to queue</button>
+        </div>
+    {/each}
+</div>
--- server/index.js
+++ server/index.js
@@ -1,40 +1,79 @@
 import express from "express";
 import { createServer } from "http";
 import { Server } from "socket.io";
+import { exec } from "child_process";
 
 const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
-  cors: {
-    origin: "http://localhost:5173",
-    methods: ["GET", "POST"]
-  }
+    cors: {
+        origin: "http://localhost:5173",
+        methods: ["GET", "POST"]
+    }
 })
 
-const roomData = {
-    "1": {
-        song: "hi"
-    }
-}
+const roomData = {};
+const apiKey = "AIzaSyBGEXCV6tyMe6jFLpY0ZzIDC-gORmM2NQU";
 
 io.on("connection", (socket) => {
-  socket.on("join-room", (roomId, callback) => {
-    if(!Object.keys(roomData).includes(roomId)) {
-      callback({
-        status: "fail",
-        reason: "Room not found!"
-      })
-      return;
-    }
-    
-    socket.join(roomId);
-    io.to(roomId).emit("room-join");
-    
-    callback({
-      status: "success",
-      roomData: roomData[roomId]
+    let currentRoom = "";
+    let searchResults = {};
+
+    socket.on("join-room", (roomId, callback) => {
+        if(!Object.keys(roomData).includes(roomId)) {
+            callback({
+                status: "fail",
+                reason: "Room not found!"
+            })
+            return;
+        } else if(socket.rooms.length > 1) {
+            callback({
+                status: "fail",
+                reason: "You are already in a room."
+            })
+            return;
+        }
+
+        currentRoom = roomId;
+        socket.join(roomId);
+        socket.to(roomId).emit("room-joined");
+
+        callback({
+            status: "success",
+            roomData: roomData[roomId]
+        })
     })
-  })
+
+    socket.on("create-room", (callback) => {
+        currentRoom = socket.id.toUpperCase();
+        roomData[currentRoom] = {
+            owner: nickname,
+            queue: [],
+            song: {}
+        }
+    })
+    
+    socket.on("add-song", (songData, callback) => {
+        roomData[currentRoom].queue.push(songData);
+    })
+
+    socket.on("search", (query, callback) => {
+        const params = new URLSearchParams({
+            part: "snippet",
+            q: query,
+            type: "video",
+            maxResults: 10,
+            order: "viewCount",
+            key: apiKey
+        })
+
+        fetch(`https://www.googleapis.com/youtube/v3/search?${params}`)
+            .then(res => res.json())
+            .then(data => {
+                searchResults = data;
+                callback(searchResults.items);
+            })
+    })
 })
 
 httpServer.listen(3000);
